f5fcc45202140661eb23f72910d8ce1b775077db
henryhoangduong <henryhoangduong@gmail.com>
2025-02-08 22:47:56 +0700

    type in chathandler

--- pkg/httpserver/chathandler.go
+++ pkg/httpserver/chathandler.go
@@ -1,1 +1,15 @@
 package httpserver
+
+type userReq struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Client   string `json:"client"`
+}
+
+type resposne struct {
+	Status  bool        `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+	Total   int         `json:"total,omitempty"`
+}
+
